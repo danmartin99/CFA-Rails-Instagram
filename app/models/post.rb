537731406eae277bde96d5b128254b0d03fd84c6
Dan Martin <dan.martin99@me.com>
2017-03-16 08:46:23 +1100

    Upadte to application.js
    add materialise-sprockets

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+#  acts_as_likeable
+
+  mount_uploader :post_image, PostImageUploader
+
+  validates :user_id, :description, presence: true
+
+  # def self.find_jamie_is_cool
+  #    where(description: 'jamie is cool')
+  #  end
+  #
+  #
+end
